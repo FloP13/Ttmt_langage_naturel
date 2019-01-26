072371d1b85ee9355d50bbca24e2a9e7da2a55f7
Thomas DUDOUX <thomas.dudoux@gmail.com>
2019-01-26 12:32:48 +0100

    Added classification report

--- testing.py
+++ testing.py
@@ -9,6 +9,7 @@ can be launched from the command line
 
 
 import numpy as np
+from sklearn.metrics import classification_report
 
 from amazon_reviews.document import AmazonReviewParser, Vectorizer
 from amazon_reviews.neural_network.recurrent import RecurrentNeuralNetwork
@@ -38,17 +39,12 @@ def _main() -> None:
     vectorizer = Vectorizer('glove.6B.50d.txt')
     word, pos, shape = vectorizer.encode_features(documents)
     labels = vectorizer.encode_annotations(documents)
-    ratings = (doc.rating for doc in documents)
     nb_features = len(word)
     print(f'Loaded {nb_features} data samples', '\n', 'Predicting...')
     model = RecurrentNeuralNetwork.load('./models_save/ner_weights.h5')
     predicted = model.predict([word, pos, shape], batch_size=64)
     predicted_classes = np.asarray([RecurrentNeuralNetwork.probas_to_classes(p) for p in predicted], dtype=np.int8)
-    accuracy = sum((1 for p, l in zip(predicted_classes, labels) if p == l)) / nb_features
-    accuracy_ratings_comments = sum((1 for p, r in zip(predicted_classes, ratings)
-                                     if _is_rating_concordant_comment(p, r))) / nb_features
-    print(f'Prediction accuracy of : {accuracy*100}%')
-    print(f'Predicted accuracy of ratings and concordant comments {accuracy_ratings_comments*100}%')
+    print(classification_report(labels, predicted_classes, ['negative', 'positive']))
 
 
 if __name__ == '__main__':
